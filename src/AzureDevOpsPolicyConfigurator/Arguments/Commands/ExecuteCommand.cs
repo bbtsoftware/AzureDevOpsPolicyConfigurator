d7ac33b8c89cd2fc6daf57d5d37157cdfb2e0d15
Elias Oehen <oee@bbtsoftware.ch>
2020-08-05 09:52:09 +0200

    Change from Execute to ExecuteAsync

--- src/AzureDevOpsPolicyConfigurator/Arguments/Commands/ExecuteCommand.cs
+++ src/AzureDevOpsPolicyConfigurator/Arguments/Commands/ExecuteCommand.cs
@@ -17,7 +17,7 @@ namespace AzureDevOpsPolicyConfigurator
         /// <param name="context">Context</param>
         /// <param name="settings">Settings</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
-        public override async Task<int> Execute(CommandContext context, ExecuterSettings settings)
+        public override async Task<int> ExecuteAsync(CommandContext context, ExecuterSettings settings)
         {
             await new PolicyExecuter(new JsonSerializer(), new FileReader(), new ConnectionProvider(), new Logger()).Execute(settings).ConfigureAwait(false);
             return 0;
